c0a991956fdc3f892a6c249b175e4fcbbc161da7
Ricardo Bittencourt <bluepenguin@gmail.com>
1997-12-30 16:49:00 -0200

    1.71: Removed bug related to D0 in sprite attribute table
    - removed bug when a 0D0h is inserted in sprite attribute table
      now salamander don't have those ugly sprites when starting
    - compiler recognizes CB and ED, but ED doesn'r works properly

--- GENCOMP.C
+++ GENCOMP.C
@@ -3,11 +3,11 @@
 void main (void) {
   int i;
 
-  printf ("info_table:\n");
+  printf ("info_table_ed:\n");
   for (i=0; i<256; i++) {
-    printf ("\t; %02X\n",i);
-    printf ("\tdd\toffset comp%02X\n",i);
-    printf ("\tdb\t(offset comp%02X_end)-(offset comp%02X)\n",i,i);
+    printf ("\t; ED%02X\n",i);
+    printf ("\tdd\t0; offset compED%02X\n",i);
+    printf ("\tdb\t0; (offset compED%02X_end)-(offset compED%02X)\n",i,i);
     printf ("\tdb\t0,0,0\n");
   }
 }
