8bc83cb9bd6142073aacf6d1c34eb9b6864763c5
Ricardo Bittencourt <bluepenguin@gmail.com>
1998-02-23 22:31:00 -0200

    1.101: Z80 is completely emulated, all opcodes
    - DSK images are no longer write-protected
    - full Z80 emulation, including all documented and undocumented opcodes
    - IM 2 emulation
    - fully operational SCREEN 1 video cache
    - fully operational save and load state
    - save and load state through the GUI
    - fixed minor bug in TV-emulation VESA2 mode
    - support for #8000 cartridges

--- GENISET.C
+++ GENISET.C
@@ -5,6 +5,6 @@ void main (void) {
 
   printf ("isetFDCBxx:\n");
   for (i=0; i<256; i++) {
-    printf ("\tisetFDCB%02X: dd emulXX\n",i);
+    printf ("\tisetFDCB%02X: dd emulFDCB%02X\n",i,i);
   }
 }
