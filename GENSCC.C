ca2f84f5b49d5c692ddc0ee6ab504348d3abad61
Ricardo Bittencourt <bluepenguin@gmail.com>
1998-08-12 22:21:00 -0300

    1.141: PSG noise, support to PC Speaker
    - faster fetch on CB DD ED FD opcodes
    - dynamic recompiler (disabled)
    - support to PSG noise
    - fixed the megaram emulation
    - optimized the sound routines
    - fixed a remaining vdp/irq bug (intro of "Zanac")
    - now LDIR can be interrupted by a irq
    - fixed a bug in mode FAST
    - more info on debug screen 2
    - changed debug commands: "A"->"F2", "L"->"F3"
    - now the debug screen is updated after a load state
    - new debug command: "0" to watch the current MSX screen
    - 3-channel sound through the speaker (option "-speaker")
    - fixed a problem in video cache ("River Raid" ,"Pay Load")
    - fixed some problems in RESET and LOAD ROM options of the GUI
    - new option in the GUI: "TURN OFF" (hard reset)

---
+++ GENSCC.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main (void) {
+  int i,j;
+
+  printf ("SCC_table:\n");
+  for (i=0; i<16; i++) {
+    printf ("; values for volume %d\n",i);
+    for (j=0; j<256; j++) {
+      printf ("\tdb\t%d\t;%02x\n",((((int)((signed char)j))+128)*i)>>8,j);
+    }
+  }
+}
